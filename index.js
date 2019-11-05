13edae27e7b4f7c535c3842f0c44909423b8cdf5
alhajiry <latiforce@gmail.com>
2019-11-05 14:44:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const digitA = 100;
+const digitB = 200.5;
+
+const resultAddition = digitA + digitB;
+const resultSubstraction = digitA - digitB;
+const resultMultiplication = digitA * digitB;
+const resultDivison = digitA / digitB;
+
+console.log(digitA);
+console.log(digitB);
+
+console.log(resultAddition);
+console.log(resultSubstraction);
+console.log(resultMultiplication);
+console.log(resultDivison);
+
+// Feel free to have some experiments with variety of operators
+// You can also using window.prompt (remember prompt value is string, so you have to parse the value to number using parseInt() or Number())
